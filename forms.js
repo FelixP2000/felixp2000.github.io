34dd14f0eb6465f8959e4b10f49a1fe15476bb64
FelixPetrov <157207899+FelixP2000@users.noreply.github.com>
2024-03-05 03:52:18 +0300

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,54 @@
+function povtori(g) {
+    var array = g.split("");
+    var rslt = 0;
+    for(i=0; i<array.length;i++){
+        if (array[i]=="@"){
+            rslt+=1;
+        }
+    }
+    return rslt;
+}
+function probeli(g) {
+    var array = g.split("");
+    var rslt = 0;
+    for(i=0; i<array.length;i++){
+        if (array[i]==" "){
+            rslt=1;
+            break;
+        }
+    }
+    return rslt;
+}
+
+window.addEventListener('submit', ()=>{
+    let formdata = new FormData(send_msg_to_author);
+
+    let mails = formdata.get('mail');
+    let names = formdata.get('names');
+    let msg = formdata.get('text_msg');
+
+    var povtoris = povtori(mails);
+    var probelis = probeli(mails);
+    if (povtoris>1 || probelis>0){
+        alert("Введите верный E-mail!");
+        return;
+    }
+    else if (mails==""){
+        alert("Введите E-mail!");
+        return;
+    }
+    else{
+        if (names==""){
+            alert("Введите своё имя!");
+            return;
+        }
+        else if (msg==""){
+            alert("Введите сообщение!");
+            return;
+        }
+        else{
+            send_msg_to_author.action = "http://f0927537.xsph.ru/";
+        }
+        
+    };
+});
