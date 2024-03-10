7027dbeb1ae9059f69a7160d4a2b1b1b270874f5
FelixP2000 <feliks.petrov.2000@mail.ru>
2024-03-10 11:24:55 +0300

    Cmd

---
+++ gits.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector('#github_but').addEventListener('click', () => {
+    document.querySelector('#code_window_git').hidden=false;
+});
+
+document.querySelector('#exit').addEventListener('click', () => {
+    document.querySelector('#code_window_git').hidden=true;
+});
+
+
+function copyes(value){
+    navigator.clipboard.writeText(value.previousElementSibling.valueOf().textContent);
+};
