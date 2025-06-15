b30e2af6e2b7ba5af4bd66729c214104f887d3f6
FelixP2000 <feliks.petrov.2000@mail.ru>
2025-06-15 04:32:43 +0300

    Edit words for Copyright

--- gits.js
+++ gits.js
@@ -2,7 +2,15 @@ document.querySelector('#github_but').addEventListener('click', () => {
     document.querySelector('#code_window_git').hidden=false;
 });
 
-document.querySelector('#exit').addEventListener('click', () => {
+document.querySelector('#linux_but').addEventListener('click', () => {
+    document.querySelector('#code_window_linux').hidden=false;
+});
+
+document.querySelector('#code_window_linux #exit').addEventListener('click', () => {
+    document.querySelector('#code_window_linux').hidden=true;
+});
+
+document.querySelector('#code_window_git #exit').addEventListener('click', () => {
     document.querySelector('#code_window_git').hidden=true;
 });
 
---
+++ replace_html.py
@@ -0,0 +1,20 @@
+import os
+
+
+files = os.listdir()
+files = [i for i in files if i.find(".html")>-1]
+
+def replace_html(x,y):
+    for j in files:
+        with open(j, "r", encoding="utf-8") as f:
+            rslt = f.read()
+            f.close()
+
+        rslt = rslt.replace(x, y)
+
+        with open(j, "w", encoding="utf-8") as f:
+            f.write(rslt)
+            f.close()
+
+
+replace_html("Copyright 2024"," Copyright 2024")
